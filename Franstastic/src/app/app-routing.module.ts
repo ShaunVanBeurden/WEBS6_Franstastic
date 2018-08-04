07c899f30d20c89d8c468bb6ce29764acc6d2e39
Unknown <smk.vanbeurden@student.avans.nl>
2018-08-04 15:31:04 +0200

    Routing setup

--- Franstastic/src/app/app-routing.module.ts
+++ Franstastic/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {CompetitionOverviewComponent} from "./competition/competition-overview/competition-overview.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'competitions', component: CompetitionOverviewComponent },
+  /*{ path: 'competition/:id', component: CompetitionDetailComponent,
+    children: [
+      { path: 'participants', component: ParticipantsComponent },
+      { path: 'pouleoverview', component: CompetitionPouleComponent },
+      { path: 'tournamentoverview', component: CompetitionTournamentComponent },
+      { path: 'knockoutoverview', component: CompetitionKnockoutComponent },
+      { path: 'matches', component: MatchDetailsComponent }
+    ]
+  },*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Franstastic/src/app/app.module.ts
+++ Franstastic/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CompetitionOverviewComponent } from './competition/competition-overview/competition-overview.component';
+import { LoginService } from "./providers/login.service";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CompetitionOverviewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
